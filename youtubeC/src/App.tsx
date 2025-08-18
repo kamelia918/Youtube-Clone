6816f87e1078c45e4c89465a63042625696a994f
kamelia918 <kameliaremaki@gmail.com>
2025-08-18 23:47:47 +0100

    fix the layout of videos and bars

--- youtubeC/src/App.tsx
+++ youtubeC/src/App.tsx
@@ -1,13 +1,17 @@
 import './App.css'
 import  Navbar from './components/NavBar/TopBar'
 import Sidebar from './components/NavBar/sideBar'
+import VideoGrid from './components/videos'
 function App() {
 
   return (
     <>
-      <Sidebar isOpen={false} />
+      
       <Navbar/>
-
+      <div className='flex'>
+        <Sidebar isOpen={true} />
+        <VideoGrid />
+      </div>
    </>
   )
 }
